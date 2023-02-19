23cbc5846eb4ab05b0b99c49fbe552babc049979
Israel <israelali142@gamil.com>
2023-02-19 16:42:37 -0500

    Learned about Props and State

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './components/model';
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [allTodos, setAllTodos] = useState<Todo[]>([]);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      // ...allTodoso : is what is already  in the todo allTodoso list,
+      // part after comma add a new todo to the list
+      setAllTodos([...allTodos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo('');
+    }
+    console.log(allTodos);
+  };
   return (
     <div className="App">
-      <h1> Hello World</h1>
+      <span className="heading"> To Do List</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
-}
+};
 
 export default App;
